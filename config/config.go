dc9bac7dde40062ebf7dc249afe1853428d14469
jamescscott <jcscott.iii@gmail.com>
2014-08-02 14:41:21 -0700

    Added simple deployed env settings picker
    Can choose between dev, qa, and prod.
    prod and qa are equal right now.
    Since you can't set the environment variable, there's
    a variable in config.go that denotes which is the current env.
    
    Also fixed the application name so that I could deploy my app.

--- config/config.go
+++ config/config.go
@@ -1,7 +1,20 @@
 package config
 
+// Typically, you read from the environment variable.
+// Can't do that on AppEngine :(
+var ENV = "dev"
+
 var BaseURL = ""
 
 func init() {
-	BaseURL = "http://localhost:8080"
+	switch ENV {
+	case "dev":
+		BaseURL = "http://localhost:8080"
+	case "qa":
+	case "prod":
+		// Insert your app's production url.
+		BaseURL = "http://testgaeappjcsiii.appspot.com/"
+	default:
+		BaseURL = ""
+	}
 }
